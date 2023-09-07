9914dc05433280441f16a554a5905b1ceabed538
Theonlysiki <135545900+Theonlysiki@users.noreply.github.com>
2023-09-07 13:06:50 +0100

    changes

--- stage1.js
+++ stage1.js
@@ -2,7 +2,9 @@ const express = require('express');
 const moment = require('moment');
 
 const app = express();
-const PORT = 3000;
+
+// Update the port to use the environment variable or default to 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/endpoint', (req, res) => {
     const slackName = req.query.slack_name;
@@ -29,5 +31,5 @@ app.get('/endpoint', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
